f081fb8c5783bccf007f614bbc1f4ec712548bd5
Bardia Moshiri <fakeshell@bardia.tech>
2024-06-11 22:24:45 -0400

    Revert "debian: drop quirks vulkan"
    This reverts commit 7549e5d09fe684265a23f0a2aa791de49082f2d1.

--- debian/gencontrol.py
+++ debian/gencontrol.py
@@ -20,6 +20,7 @@ COMMON_PACKAGES = [
 	"furios-quirks-qt-force-gles",
 	"furios-quirks-tls-padding",
 	"furios-quirks-xtables-legacy",
+	"furios-quirks-vulkan",
 	"gstreamer1.0-droid",
 	"package-sideload",
 	"flash-bootimage",
